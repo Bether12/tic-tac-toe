ae6898329554059a71b7490503239b32d1fe5ec2
Bether12 <elnenogomez13@gmail.com>
2026-02-13 08:47:30 -0500

    Add last three cases

--- script.js
+++ script.js
@@ -42,6 +42,18 @@ const gameBoard =(function(){
         else if(board[2][0]!==" " && board[2][0] === board[2][1] && board[2][0] === board[2][2]){
             return board[2][0];
         }
+        //left column check
+        else if(board[0][0]!==" " && board[0][0] === board[1][0] && board[0][0] === board[2][0]){
+            return board[0][0];
+        }
+        //center column check
+        else if(board[0][1]!==" " && board[0][1] === board[1][1] && board[0][1] === board[2][1]){
+            return board[0][1];
+        }
+        //right column check
+        else if(board[0][2]!==" " && board[0][2] === board[1][2] && board[0][2] === board[2][2]){
+            return board[0][2];
+        }    
         //upper left to lower right diagonal check
         else if(board[0][0]!==" " && board[0][0] === board[1][1] && board[0][0] === board[2][2]){
             return board[0][0];
